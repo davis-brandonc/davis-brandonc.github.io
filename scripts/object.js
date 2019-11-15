f8c0d61980fc5751205bb07931203ef293dde5b6
Davis-Brandon <bdavis29@byui.edu>
2019-11-14 20:46:01 -0700

    Create object.js

---
+++ scripts/object.js
@@ -0,0 +1,3 @@
+var movie = {movieName:"Gaurdians of the Galaxy", studioName:"Disney", releaseYear:2014, genre:"Action"};
+document.getElementById("object").innerHTML =
+movie.movieName + " is an " + movie.genre + " movie that was released in " + movie.releaseYear;

f0c1475946b469c34df5c98a532242ad2a02a7f5
Davis-Brandon <bdavis29@byui.edu>
2019-11-14 20:46:11 -0700

    Create variables.js

---
+++ scripts/variables.js
@@ -0,0 +1,6 @@
+var value1 = 9;
+var value2 = 10;
+var value3 = 7;
+var value4 = 6;
+var total = value1 + value2 + value3 - value4;
+document.getElementById("variable").innerHTML = "The total is " + total;

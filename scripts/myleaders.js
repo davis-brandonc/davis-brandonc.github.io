c8f222785458c031de07ca28109067865c786fd7
Davis-Brandon <bdavis29@byui.edu>
2019-11-20 20:54:41 -0700

    Create myleaders.js

---
+++ scripts/myleaders.js
@@ -0,0 +1,6 @@
+function myLeaders() {
+  var node = document.createElement("LI");
+  var textnode = document.createTextNode("586 - Frank Robinson");
+  node.appendChild(textnode);
+  document.getElementById("myHomers").appendChild(node);
+    }
